82129dcd783f69e1c78ffdff5030528fb86536b5
halwohamda <halwohamda@gmail.com>
2024-02-09 19:18:30 -0800

    done

--- coffee/src/App.jsx
+++ coffee/src/App.jsx
@@ -1,43 +1,43 @@
-// import React from 'react'
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import Menu from './components/Menu';
-// import About from './components/About'
-// import Products from './components/Products';
-// import Review from './components/Review';
-// import Footer from './components/Footer';
+import React from 'react'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import About from './components/About'
+import Products from './components/Products';
+import Review from './components/Review';
+import Footer from './components/Footer';
 
-// const App = () => {
-//   return (
-//     <div>
-//       <Navbar/>
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
 
 
-//       <main>
-//         <div id='Home'>
-//           <Home />
-//         </div>
-//         <div id='Menu'>
-//        <Menu/>
-//         </div>
-//       <div id='About'>
-//         <About />
-//       </div>
+      <main>
+        <div id='Home'>
+          <Home />
+        </div>
+        <div id='Menu'>
+       <Menu/>
+        </div>
+      <div id='About'>
+        <About />
+      </div>
 
-//       <div id='Products'>
-//          < Products />
-//       </div>
+      <div id='Products'>
+         < Products />
+      </div>
 
-//       <div id='Review'>
-//         <Review />
-//       </div>
+      <div id='Review'>
+        <Review />
+      </div>
 
-//       <div id='Footer'>
-//         <Footer />
-//       </div>
-//       </main>
-//     </div>
-//   )
-// }
+      <div id='Footer'>
+        <Footer />
+      </div>
+      </main>
+    </div>
+  )
+}
 
-// export default App
+export default App
