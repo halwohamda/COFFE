b7109533060d5512811545f1903ac052a68d10c5
halwohamda <halwohamda@gmail.com>
2024-02-09 19:19:24 -0800

    done

--- coffee/src/layouts/Button.jsx
+++ coffee/src/layouts/Button.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
+import React from 'react'
 
-// const Button = (props) => {
-//   return (
-//     <div>
-//         <button className='px-6 py-1 border-white bg-[#FFDCAB] hover:text-[#AB6B2E] transition-all rounded-full border-2 '>
-//                 {props.title}
-//         </button>
-//     </div>
-//   )
-// }
+const Button = (props) => {
+  return (
+    <div>
+        <button className='px-6 py-1 border-white bg-[#FFDCAB] hover:text-[#AB6B2E] transition-all rounded-full border-2 '>
+                {props.title}
+        </button>
+    </div>
+  )
+}
 
-// export default Button
+export default Button
