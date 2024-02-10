5e666cad0250035671719eee307ef4321039d15d
halwohamda <halwohamda@gmail.com>
2024-02-09 19:19:49 -0800

    done

--- coffee/src/layouts/ProductsCard.jsx
+++ coffee/src/layouts/ProductsCard.jsx
@@ -1,26 +1,26 @@
-// import React from 'react'
-// import Button from './Button';
-// import {BsStarFill,BsStarHalf} from 'react-icons/bs';
+import React from 'react'
+import Button from './Button';
+import {BsStarFill,BsStarHalf} from 'react-icons/bs';
 
-// const ProductsCard = (props) => {
-//   return (
-//     <div className='w-full lg:w-1/4 bg-white p-3 rounded-lg '>
-//         <img className='rounded-lg ' src={props.img} alt="" />
+const ProductsCard = (props) => {
+  return (
+    <div className='w-full lg:w-1/4 bg-white p-3 rounded-lg '>
+        <img className='rounded-lg ' src={props.img} alt="" />
 
-//         <div className='flex flex-col items-center mt-5 gap-3 '>
-//                 <h2 className='font-semibold text-xl'>{props.title}</h2>
-//                 <div className='flex '>
-//                         <BsStarFill className="text-brightColor " />
-//                         <BsStarFill className="text-brightColor " />
-//                         <BsStarFill className="text-brightColor " />
-//                         <BsStarFill className="text-brightColor " />
-//                         <BsStarHalf className="text-brightColor " />
-//                 </div>
-//                 <h3 className='font-semibold text-lg'>$15.99</h3>
-//                 <Button title='ADD TO CARD' />
-//         </div>
-//     </div>
-//   )
-// }
+        <div className='flex flex-col items-center mt-5 gap-3 '>
+                <h2 className='font-semibold text-xl'>{props.title}</h2>
+                <div className='flex '>
+                        <BsStarFill className="text-brightColor " />
+                        <BsStarFill className="text-brightColor " />
+                        <BsStarFill className="text-brightColor " />
+                        <BsStarFill className="text-brightColor " />
+                        <BsStarHalf className="text-brightColor " />
+                </div>
+                <h3 className='font-semibold text-lg'>$15.99</h3>
+                <Button title='ADD TO CARD' />
+        </div>
+    </div>
+  )
+}
 
-// export default ProductsCard
+export default ProductsCard
